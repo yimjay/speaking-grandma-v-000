1ad2eb2f6fd697ac59c28a9ac731c3e3a41fb83d
Ji Yim <gsooyim@gmail.com>
2018-04-11 02:05:32 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.
+  elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
